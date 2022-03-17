f35c641b3b84b68ac7f7a2eba60725e6605f5ad0
Samuel Lyons <samuellyons@Samuels-MacBook-Air.local>
2022-03-17 13:47:32 -0400

    first rendition of my portfolio

---
+++ app.js
@@ -0,0 +1,53 @@
+const containerOne = document.getElementById("container-1");
+const containerTwo = document.getElementById("container-2");
+const containerFive = document.getElementById("container-5");
+const containerSix = document.getElementById("container-6");
+const containerNine = document.getElementById("container-9");
+const containerTen = document.getElementById("container-10");
+const containerEleven = document.getElementById("container-11");
+const containerTwelve = document.getElementById("container-12");
+
+
+
+contact.addEventListener('click', (e)=> {
+    contact.classList.toggle(contactChange);
+})
+containerOne.addEventListener('click', (e)=> {
+    window.location='https://slyons2397.github.io/odin-recipes/'
+})
+
+containerTwo.addEventListener('click', (e)=> {
+    window.location='https://slyons2397.github.io/odin-website-project/'
+
+})
+
+containerFive.addEventListener('click', (e)=> {
+    window.location='https://slyons2397.github.io/SignUpProject/'
+
+})
+
+containerSix.addEventListener('click', (e)=> {
+    window.location='https://slyons2397.github.io/AdminPage/'
+
+})
+
+containerNine.addEventListener('click', (e)=> {
+    window.location='https://slyons2397.github.io/RockPaperScissorsV2/'
+
+})
+
+containerTen.addEventListener('click', (e)=> {
+    window.location='https://slyons2397.github.io/PeopleCounter/'
+
+})
+
+containerEleven.addEventListener('click', (e)=> {
+    window.location='https://slyons2397.github.io/Calculator/'
+
+})
+
+
+containerTwelve.addEventListener('click', (e)=> {
+    window.location='https://slyons2397.github.io/booklist/'
+
+})
